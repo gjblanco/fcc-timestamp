889d1193f66ab336a5d8115348393df4731f52f9
guille blanco <guille blanco>
2017-06-17 17:10:50 -0500

    finish

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/:request', function(req, res){
+  var request = req.params.request;
+  var date = new Date(request);
+  res.send({ unix: date.getTime() / 1000, natural: date.toLocaleString('en-US', {year: 'numeric', month: "long", day: "numeric"}) })
+})
+
+app.get('/', function(req, res) {
+  res.send('./public/index.html');
+});
+
+app.listen(4567, function() {
+  console.log('listening on port 4567...');
+})
