040ad5d43836312730bec49ff825996d576bdd0b
guille blanco <guille blanco>
2017-06-17 17:48:18 -0500

    fix bug

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ app.use(bodyParser.json());
 
 app.get('/:request', function(req, res){
   var request = req.params.request;
+  if(!isNaN(request)) {
+    request = (+request) * 1000;
+  }
   var date = new Date(request);
+  
+  date = new Date(date.getTime() + (new Date('January 1 1970') - new Date(0)));
+  //res.send(date.toString())
   res.send({ unix: date.getTime() / 1000, natural: date.toLocaleString('en-US', {year: 'numeric', month: "long", day: "numeric"}) })
 })
 
